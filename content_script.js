01efc708b4809064f7e02e18859ee001e599d4b5
Ansh Mehta <40193797+ansh-mehta@users.noreply.github.com>
2021-06-03 01:43:58 +0530

    Add files via upload

---
+++ content_script.js
@@ -0,0 +1,17 @@
+var timer1;
+
+window.onhashchange = applyBackground();
+
+function applyBackground() {
+    timer1 = setInterval(findElement1, 1000);    
+}
+//Feed, Network, Notifications, Profile, Jobs, Messaging
+function findElement1(){
+    var element = document.getElementsByTagName("body")[0];
+    if (element!=null){
+        element.style.backgroundImage="linear-gradient(90deg, rgb(255, 0, 0,0.5),rgb(255, 165, 0,0.5), rgb(255, 255, 0,0.5), rgb(0, 128, 0,0.5), rgb(0, 0, 255,0.5), rgb(75, 0, 130,0.5), rgb(238, 130, 238,0.5))";
+        clearInterval(timer1);
+    }
+}
+
+applyBackground();
