2c54e1f42a3d22de500d3cb50461073b78d5b2ed
Ansh Mehta <40193797+ansh-mehta@users.noreply.github.com>
2021-06-03 02:36:10 +0530

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -9,7 +9,7 @@ function applyBackground() {
 function findElement1(){
     var element = document.getElementsByTagName("body")[0];
     if (element!=null){
-        element.style.backgroundImage="linear-gradient(90deg, rgb(255, 0, 0,0.5),rgb(255, 165, 0,0.5), rgb(255, 255, 0,0.5), rgb(0, 128, 0,0.5), rgb(0, 0, 255,0.5), rgb(75, 0, 130,0.5), rgb(238, 130, 238,0.5))";
+        element.style.backgroundImage="linear-gradient(90deg, rgb(255, 0, 0,0.55),rgb(255, 165, 0,0.55), rgb(255, 255, 0,0.55), rgb(0, 128, 0,0.55), rgb(0, 0, 255,0.55), rgb(75, 0, 130,0.55), rgb(238, 130, 238,0.55))";
         clearInterval(timer1);
     }
 }
